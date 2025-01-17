5be65a374955f19af0bf2f993f642cca39424d02
salcedoma <salcedoma.github@gmail.com>
2025-01-17 17:13:20 -0300

    Add function to read value of button when clicked

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
-let cpuPlay = document.getElementById('cpu-play');
-let winner = document.getElementById('winner');
-let moves = document.getElementById('moves');
+const cpuPlay = document.getElementById('cpu-play');
+const winner = document.getElementById('winner');
+const moves = document.getElementById('moves');
 
+
+
+document.querySelector('#moves').addEventListener('click', event => { //I would've used document.getElementById('moves')
+    let btnClicked = event.target;
+    if (btnClicked.matches('button')) { //could've used .tagName instead of .matches
+      let value = btnClicked.value;
+      console.log(value);
+      return value;
+    }
+  });
 
 function random() {
     return Math.floor(Math.random() * 3);
@@ -10,17 +20,17 @@ function random() {
 function rps(number) {
     switch (number) {
         case 0:
-            console.log("rock");
+            // console.log("rock");
             cpuPlay.innerText = "rock";
             return "rock";
             break;
         case 1:
-            console.log("paper");
+            // console.log("paper");
             cpuPlay.innerText = "paper";
             return "paper";
             break;
         case 2:
-            console.log("scissors");
+            // console.log("scissors");
             cpuPlay.innerText = "scissors";
             return "scissors";
             break;
