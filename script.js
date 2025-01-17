a1e07da81c20e122ad4ebae463897911685de7d3
salcedoma <salcedoma.github@gmail.com>
2025-01-17 16:13:15 -0300

    Get html elements to change cpu play and winner text
    Add id to necesary tags, to be able to call them in the js code.

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-console.log("hello world")
+let cpuPlay = document.getElementById('cpu-play');
+let winner = document.getElementById('winner')
+
 
 function random() {
     return Math.floor(Math.random() * 3);
@@ -8,14 +10,17 @@ function rps(number) {
     switch (number) {
         case 0:
             console.log("rock");
+            cpuPlay.innerText = "rock";
             return "rock";
             break;
         case 1:
             console.log("paper");
+            cpuPlay.innerText = "paper";
             return "paper";
             break;
         case 2:
             console.log("scissors");
+            cpuPlay.innerText = "scissors";
             return "scissors";
             break;
     } 
